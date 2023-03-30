e1b225b606aa97b8fe9dd50a12aa59309eaa8e39
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-03-30 16:45:30 -0400

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,7 @@
+#Write a program to display only numbers from a list that satisfy the following
+#conditions:
+#● The number must be greater than 10.
+#● The number must be even.
+#● If the number is 32, skip it and keep going.
+#● If the number is greater than 50, then stop the loop.
+#● List = [2, 5, 13, 18, 22, 26, 29, 30, 32, 35, 37, 40, 41, 44, 50, 52, 56]
